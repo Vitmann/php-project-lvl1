60e907e0776444a06d119f0d1a8ac435d5850367
Vitaly Tarasov <89269643535@mail.ru>
2022-10-06 22:59:17 +0300

    5 step

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Vitmann/php-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Vitmann/php-project-lvl1/actions)
 <a href="https://codeclimate.com/github/Vitmann/php-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/f7da0a2ef388e27bd07e/maintainability" /></a>
+
+
+https://asciinema.org/a/svqjZ96hR8Ts01pmOJJZw3nl9
--- src/Cli.php
+++ src/Cli.php
@@ -10,4 +10,26 @@ function Hello()
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+
+    $number = [15, 6, 7];
+
+    foreach ($number as $num) {
+        line($num);
+        $answer = prompt('Enter your answer');
+        if ($num % 2 === 0){
+            $even = 'yes';
+        } else {
+            $even = 'no';
+        }
+
+        if ($even ===  $answer ) {
+            line('Correct!');
+        } else {
+            line($answer .  ' is wrong answer ;(. Correct answer was ' . $even);
+            break;
+        }
+
+        line('Congratulations,' . $name . '!');
+    }
 }
