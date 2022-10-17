0d9ac89721a316c36de9a5e03d2ac0c9ebc66dfd
Vitaly Tarasov <89269643535@mail.ru>
2022-10-17 17:58:19 +0300

    Prime or not

---
+++ src/Games/Prime.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BrainGames\Prime;
+
+use function cli\line;
+use function cli\prompt;
+
+function primeCheck($number): string
+{
+    if ($number == 1) {
+        return 'no';
+    }
+
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i == 0) {
+            return 'no';
+        }
+    }
+    return 'yes';
+}
+
+function Prime()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+    $arrNumber = [2, 3, 5, 7, 11, 13, 17, 19, 23, 1, 4, 6, 8];
+
+    for ($a = 0; $a < 3; ++$a) {
+        $number = $arrNumber[array_rand($arrNumber, 1)];
+
+        line('Question: ' . $number);
+        $answer = prompt('Your answer');
+
+        if ($answer == primeCheck($number)) {
+            line('Correct!');
+        } else {
+            line($answer . ' is wrong answer ;(. Correct answer was ' . primeCheck($number));
+            line("Let's try again, " . $name);
+            break;
+        }
+    }
+}
