6bf64bef6c1fca644c64d2f03b04b84414d13bfa
Vitaly Tarasov <89269643535@mail.ru>
2022-10-17 21:22:48 +0300

    Prime or not

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -19,14 +19,14 @@ function primeCheck($number): string
     return 'yes';
 }
 
-function Prime()
+function Prime(): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-    $arrNumber = [2, 3, 5, 7, 11, 13, 17, 19, 23, 1, 4, 6, 8];
+    $arrNumber = array(2, 3, 5, 7, 11, 13, 17, 19, 23, 1, 4, 6, 8);
 
     for ($a = 0; $a < 3; ++$a) {
         $number = $arrNumber[array_rand($arrNumber, 1)];
