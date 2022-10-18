661998d6ecd13bcef05f9b824d2b49f472da3506
Vitaly Tarasov <89269643535@mail.ru>
2022-10-18 11:47:21 +0300

    bug fix

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -5,6 +5,14 @@ namespace BrainGames\Gcd;
 use function cli\line;
 use function cli\prompt;
 
+function get_greatest_common_divisor( $a, $b ) {
+    $large = $a > $b ? $a: $b;
+    $small = $a > $b ? $b: $a;
+    $remainder = $large % $small;
+    return 0 == $remainder ? $small : get_greatest_common_divisor( $small, $remainder );
+}
+
+
 function Gcd()
 {
     line('Welcome to the Brain Game!');
@@ -16,12 +24,12 @@ function Gcd()
         $num1 = rand(1, 10);
         $num2 = rand(1, 10);
         line('Question: ' . $num1 . ' ' . $num2);
-        $answer = prompt('Your answer: ');
+        $answer = prompt('Your answer ');
 
-        if ($answer == gmp_gcd($num1, $num2)) {
+        if ($answer == get_greatest_common_divisor($num1, $num2)) {
             line('Correct!');
         } else {
-            line($answer . ' is wrong answer ;(. Correct answer was ' . gmp_gcd($num1, $num2));
+            line($answer . ' is wrong answer ;(. Correct answer was ' . get_greatest_common_divisor($num1, $num2));
             line("Let's try again, " . $name . '!');
             return;
         }
