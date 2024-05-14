83dfdea29a35cb5317683b4c57ac271afdadfba5
Vitaly Tarasov <89269643535@mail.ru>
2024-05-14 07:20:25 +0300

    опять

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -12,17 +12,17 @@ const MAX_RANDOM_NUMBER = 10;
 //Игра: "Проверка на чётность"
 function runGameEven(): void
 {
-    $task = [];
-    for ($a = 0; $a < ROUNDS_COUNT; ++$a) {
-        $task[] = [
+    $taskEven = [];
+    for ($index = 0; $index < ROUNDS_COUNT; ++$index) {
+        $taskEven[] = [
             'questionText' => 'Answer "yes" if the number is even, otherwise answer "no".',
             'question' => rand(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER),
             'answer' => null
         ];
 
-        $task[$a]['answer'] = isEven($task[$a]['question']) ? 'yes' : 'no';
+        $taskEven[$index]['answer'] = isEven($taskEven[$index]['question']) ? 'yes' : 'no';
     }
-    play($task);
+    play($taskEven);
 }
 
 function isEven(int $number): bool
